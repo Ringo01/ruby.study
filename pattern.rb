a4ed7f720f4baf97de4d3f15dc0b8c17155e73ad
Ringo01 <758587439@qq.com>
2017-12-15 09:37:51 +0800

    Update pattern.rb

---
+++ pattern.rb
@@ -0,0 +1,11 @@
+line1 = "Cats are smarter than dogs"
+line2 = "Dogs also like meats"
+
+if line1 =~ /Cats(.*)/
+  puts "line1 contants Cats"
+end
+if line2 =~ /Cats(.*)/
+
+  puts "line2 contants Cats"
+end
+
